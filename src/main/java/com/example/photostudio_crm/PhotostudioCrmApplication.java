feeed6d366c8680f2b27f3b00386561c24ed9994
Yarik <yaroslav.matsapura@gmail.com>
2022-12-16 22:44:34 +0200

    Initial commit

---
+++ src/main/java/com/example/photostudio_crm/PhotostudioCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.photostudio_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotostudioCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotostudioCrmApplication.class, args);
+    }
+
+}
